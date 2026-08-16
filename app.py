8b7128f5bd6f129e22d5f02e39f5ab8d890e1f6e
Juan David Moreno Suarez <juansua080301@gmail.com>
2026-08-15 20:08:57 -0600

    Actualizo catalogo y contacto de WhatsApp

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ perfumes = {
         "marca": "Rayhaan",
         "imagen": "rayhaan-aquatica.jpeg",
         "descripcion": "Fresco, acuático y perfecto para uso diario.",
-        "precio": "$1,400 MXN",
+        "precio": "Botella completa bajo pedido",
         "decant_5": "$90 MXN",
         "decant_10": "$160 MXN",
         "categoria": "arabe"
@@ -20,7 +20,7 @@ perfumes = {
         "marca": "Rayhaan",
         "imagen": "rayhaan-elixir.jpeg",
         "descripcion": "Dulce, elegante y con gran presencia.",
-        "precio": "$1,000 MXN",
+        "precio": "Botella completa bajo pedido",
         "decant_5": "$80 MXN",
         "decant_10": "$140 MXN",
         "categoria": "arabe"
@@ -31,7 +31,7 @@ perfumes = {
         "marca": "Lattafa",
         "imagen": "honor-glory.jpeg",
         "descripcion": "Piña, vainilla y especias con un estilo dulce.",
-        "precio": "$750 MXN",
+       "precio": "Botella completa bajo pedido",
         "decant_5": "$60 MXN",
         "decant_10": "$110 MXN",
         "categoria": "arabe"
@@ -42,7 +42,7 @@ perfumes = {
         "marca": "French Avenue",
         "imagen": "spectre-wraith.jpeg",
         "descripcion": "Dulce, misterioso y con un estilo diferente.",
-        "precio": "$1,000 MXN",
+        "precio": "Botella completa bajo pedido",
         "decant_5": "$80 MXN",
         "decant_10": "$140 MXN",
         "categoria": "arabe"
@@ -53,18 +53,40 @@ perfumes = {
         "marca": "Lattafa",
         "imagen": "khamrah-qahwa.jpeg",
         "descripcion": "Fragancia dulce con café, especias y vainilla.",
-        "precio": " $800 MXN",
+        "precio": "Botella completa bajo pedido",
         "decant_5": "$70 MXN",
         "decant_10": "$120 MXN",
         "categoria": "arabe"
     },
+
+    "afnan-9am-dive": {
+    "nombre": "Afnan 9AM Dive",
+    "marca": "Afnan",
+    "imagen": "afnan-9am-dive.jpeg",
+    "descripcion": "Una fragancia masculina fresca, acuática y cítrica, con un estilo limpio, moderno y versátil.",
+    "precio": "Botella completa bajo pedido",
+    "decant_5": "$55 MXN",
+    "decant_10": "$100 MXN",
+    "categoria": "arabe"
+},
+
+"lattafa-asad": {
+    "nombre": "Lattafa Asad",
+    "marca": "Lattafa",
+    "imagen": "lattafa-asad.jpeg",
+    "descripcion": "Una fragancia masculina cálida, especiada y ambarada, con un carácter intenso, dulce y elegante.",
+    "precio": "Botella completa bajo pedido",
+    "decant_5": "$60 MXN",
+    "decant_10": "$110 MXN",
+    "categoria": "arabe"
+},
 
     "yara-tous": {
     "nombre": "Lattafa Yara Tous Women EDP",
     "marca": "Lattafa",
     "imagen": "yara-tous.jpeg",
     "descripcion": "Una fragancia femenina dulce, tropical y cremosa con un toque elegante y sofisticado.",
-    "precio": "$650 MXN",
+    "precio": "Botella completa bajo pedido",
     "decant_5": "$55 MXN",
     "decant_10": "$100 MXN",
     "categoria": "arabe"
@@ -75,7 +97,7 @@ perfumes = {
     "marca": "Lattafa",
     "imagen": "lattafa-eclaire.jpeg",
     "descripcion": "Una fragancia femenina dulce y cremosa con notas gourmand de vainilla, caramelo y leche, elegante y reconfortante.",
-    "precio": "$700 MXN",
+    "precio": "Botella completa bajo pedido",
     "decant_5": "$60 MXN",
     "decant_10": "$110 MXN",
     "categoria": "arabe"
@@ -89,7 +111,7 @@ perfumes = {
         "marca": "Versace",
         "imagen": "eros-edp.jpeg",
         "descripcion": "Dulce, fresco y con estilo juvenil.",
-        "precio": "$1,850 MXN",
+        "precio": "Botella completa bajo pedido",
         "decant_5": "$110 MXN",
         "decant_10": "$210 MXN",
         "categoria": "disenador"
@@ -100,7 +122,7 @@ perfumes = {
         "marca": "Versace",
         "imagen": "dylan-blue.jpeg",
         "descripcion": "Fresco, elegante y versátil.",
-        "precio": "$1,550 MXN",
+        "precio": "Botella completa bajo pedido",
         "decant_5": "$100 MXN",
         "decant_10": "$180 MXN",
         "categoria": "disenador"
@@ -111,11 +133,33 @@ perfumes = {
         "marca": "Azzaro",
         "imagen": "the-most-wanted.jpeg",
         "descripcion": "Fragancia dulce, intensa y masculina. Perfecta para noches y ocasiones especiales.",
-        "precio": "$1,500 MXN",
-        "decant_5": "$110 MXN",
+        "precio": "Botella completa bajo pedido",
+        "decant_5": "$100 MXN",
         "decant_10": "$180 MXN",
         "categoria": "disenador"
     },
+
+    "montblanc-explorer": {
+    "nombre": "Montblanc Explorer EDP",
+    "marca": "Montblanc",
+    "imagen": "montblanc-explorer.jpeg",
+    "descripcion": "Una fragancia masculina fresca, amaderada y elegante, ideal para el uso diario y ocasiones especiales.",
+    "precio": "Botella completa bajo pedido",
+    "decant_5": "$80 MXN",
+    "decant_10": "$150 MXN",
+    "categoria": "diseñador"
+},
+
+"lacoste-l1212-noir": {
+    "nombre": "Lacoste L.12.12 Noir",
+    "marca": "Lacoste",
+    "imagen": "lacoste-l1212-noir.jpeg",
+    "descripcion": "Una fragancia masculina fresca, dulce y amaderada, con un estilo casual y elegante.",
+    "precio": "Botella completa bajo pedido",
+    "decant_5": "$80 MXN",
+    "decant_10": "$150 MXN",
+    "categoria": "diseñador"
+},
 
 
     "swy-intensely": {
@@ -123,7 +167,7 @@ perfumes = {
         "marca": "Emporio Armani",
         "imagen": "swy-intensely.jpeg",
         "descripcion": "Dulce, cálido y elegante, ideal para noches frías.",
-        "precio": "$2,600 MXN",
+        "precio": "Botella completa bajo pedido",
         "decant_5": "$160 MXN",
         "decant_10": "$300 MXN",
         "categoria": "disenador"
